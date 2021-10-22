7c1bbf24a70175fe6cb1eb6a7bd6fa2e27bd9073
DrAtomic <leo.ward.garcia@gmail.com>
2021-10-21 23:38:16 -0700

    deleted modify face, masked lower and upper face

--- src/detect_face.py
+++ src/detect_face.py
@@ -5,7 +5,7 @@ import cv2
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("../data/shape_predictor_68_face_landmarks.dat")
 
-def create_box(img,points):
+def create_mask(img,points):
     """creates a box around a set of points
 
     Args:
@@ -18,12 +18,42 @@ def create_box(img,points):
     """
     zeros = np.zeros(img.shape,dtype=np.uint8)
     mask = cv2.fillPoly(zeros,[points],(255,255,255))
-    # img = cv2.bitwise_and(img,mask)
-    # bounding_box = cv2.boundingRect(points)
-    # x,y,w,h = bounding_box
-    # crop = img[y:y+h,x:x+w]
-    # resize = cv2.resize(crop,(0,0),None,5,5)
     return mask
+
+def top_mask(img,forhead):
+    """this makes a mask from the forhead up
+
+    Args:
+       img:       img to mask
+       forhead:   forhead point
+
+    Returns:
+        mask of the top half and bottom half
+
+    """
+    zeros = np.zeros(img.shape,dtype=np.uint8)
+    forhead_mask = cv2.circle(zeros, forhead, 125,(255,255,255),-1)
+    
+    return forhead_mask
+
+def get_region(img,points):
+    """gets a region of the face of the face with the most flesh tone
+
+    Args:
+       img: croped image of face
+       points: face points
+
+    Returns:
+        region of fleshy face
+
+    """
+
+    # get a bounding box from 41 to 1
+    #                          1 to 6
+    #                          6 to 49
+    #                          49 to 41
+    pass
+    
 
 def edit_face(img,mask):
     """applies the color to the face
@@ -42,7 +72,6 @@ def edit_face(img,mask):
 img = cv2.imread("../data/2021-10-20-224934.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = detector(gray)
-
 for face in faces:
     x1,y1 = face.left(),face.top()
     x2,y2 = face.right(),face.bottom()
@@ -53,13 +82,23 @@ for face in faces:
         x = landmarks.part(i).x
         y = landmarks.part(i).y
         face_points.append([x,y])
-        #cv2.circle(img,(x,y),2,(0,0,255),2)
-        #cv2.putText(img,str(i),(x,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255),1)
+        cv2.circle(img,(x,y),2,(0,0,255),2)
+        cv2.putText(img,str(i),(x,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255),1)
+
 
     face_points = np.array(face_points)
-    region = create_box(img,face_points[0:17])
+    regions = []
+    regions.append(create_mask(img,face_points[0:17]))
+    regions.append(top_mask(img,face_points[28]))
 
-    img = edit_face(img,region)
+    
+    
+    cv2.imshow("test1",img)
+    img = edit_face(img,regions[0])
+    img = edit_face(img,regions[1])
+    
+
+
     
 cv2.imshow("test",img)
 cv2.waitKey(0)
--- src/stream_funcs.py
+++ src/stream_funcs.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-from detect_face import find_face
-from modify_face import modify_face
+
+
 
 
 def capture_cam():
@@ -13,29 +13,10 @@ def capture_cam():
     
     while True:
         ret_val, img = cam.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        faces = find_face(gray)
 
-        if faces != []:
-            modify_face(img,faces)
         cv2.imshow('no face', img)
         
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
     cam.release()
     cv2.destroyAllWindows()
-
-
-def still_image():
-
-    img = cv2.imread("../data/2021-10-20-224934.jpg")
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    faces = find_face(gray)
-    edges, contours = modify_face(img,faces)
-    cv2.imshow("edges", edges)
-    cv2.imshow("contours",contours)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-still_image()
-
