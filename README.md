43d1c84272ac2caf4fcad58c1648c0d0c3739100
DrAtomic <leo.ward.garcia@gmail.com>
2021-10-22 21:25:46 -0700

    update readme cleaned main added todo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-we are doing a face removal. spooooooky
+# Description
+
+this program finds a faces using dlib and creates an ellipse mask with a flesh tone over the persons face. its not very good as is. theres a few things that need to get done. 
+
+# get the code
+
+```
+git clone https://github.com/DrAtomic/no-face.git
+cd no-face
+pip install -r requirements.txt
+```
+
+# run the code
+
+```
+python src/detect_face.py
+```
+
+# example
+
+![no-face](./data/screenshot.png)
+
+## note
+i intentionally pushed the landmarks data because i thought it was hard to find. when you google "dlib landmarks data set" you get a bunch of tutorials and none of them have the data set either. 
--- src/detect_face.py
+++ src/detect_face.py
@@ -2,10 +2,6 @@ import dlib
 import numpy as np
 import cv2
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("../data/shape_predictor_68_face_landmarks.dat")
-
-cam = cv2.VideoCapture(0)
 
 def get_region(img,point):
     """gets a region of the face of the face with the most flesh tone
@@ -23,6 +19,7 @@ def get_region(img,point):
     region = img[point[1]-h:point[1], point[0]-w:point[0]]
     return region
 
+
 def get_avg(region):
     """gets the avg pixel value of fleshy region
 
@@ -36,6 +33,7 @@ def get_avg(region):
     average = (region.sum(axis=1).sum(axis=0)) / (region.shape[0] * region.shape[1])
     return average
 
+
 def create_mask(img,point,color):
     """creates a box around a set of points
 
@@ -48,8 +46,11 @@ def create_mask(img,point,color):
         mask
 
     """
+    # TODO: make mask a better, reincorporate face points from 7c1bbf24a7
+    # TODO: create a two part mask, a top and a bottom
     zeros = np.zeros(img.shape,dtype=np.uint8)
-    mask = cv2.ellipse(zeros,point,(150,125),90,0,360,color,-1)
+    # TODO: make the axis a function of the area from the facepoints
+    mask = cv2.ellipse(zeros,point,(125,93),90,0,360,color,-1)
     return mask
 
 def edit_face(img):
@@ -62,38 +63,38 @@ def edit_face(img):
         faceless person
 
     """
-    
+
     white_mask = create_mask(img,face_points[29],(255,255,255))
     white_region_face = cv2.bitwise_or(img,white_mask)
-
+    #TODO: mask is a bit abrupt, apply blur to smooth the edges
     flesh_region = get_region(img,face_points[30])
     flesh_avg = get_avg(flesh_region)
-
     img[np.all(white_region_face == (255,255,255),axis=-1)] = flesh_avg
-
     return img
 
-while True:
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray)
-    for face in faces:
-        x1,y1 = face.left(),face.top()
-        x2,y2 = face.right(),face.bottom()
-        #img = cv2.rectangle(img, (x1,y1),(x2,y2),(0,0,255),2)
-        landmarks = predictor(gray,face)
-        face_points = []
-        for i in range(len(landmarks.parts())):
-            x = landmarks.part(i).x
-            y = landmarks.part(i).y
-            face_points.append([x,y])
-            # cv2.circle(img,(x,y),2,(0,0,255),2)
-            # cv2.putText(img,str(i),(x,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255),1)
+if __name__ == '__main__':
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("../data/shape_predictor_68_face_landmarks.dat")
+    cam = cv2.VideoCapture(0)
+    
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray)
+        for face in faces:
+            landmarks = predictor(gray,face)
+            face_points = []
+            for i in range(len(landmarks.parts())):
+                x = landmarks.part(i).x
+                y = landmarks.part(i).y
+                face_points.append([x,y])
+                # cv2.circle(img,(x,y),2,(0,0,255),2)
+                # cv2.putText(img,str(i),(x,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255),1)
 
-    face_points = np.array(face_points)
-    img = edit_face(img)
-    cv2.imshow("noface",img)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-cam.release()
-cv2.destroyAllWindows()
+        face_points = np.array(face_points)
+        img = edit_face(img)
+        cv2.imshow("noface",img)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+    cam.release()
+    cv2.destroyAllWindows()
